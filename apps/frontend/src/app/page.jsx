fd95e188aa3b97a41b47ab8164da5f390e0ab193
Semyon <125258352+SSUHOY@users.noreply.github.com>
2024-02-12 21:20:02 +0700

    refactors

--- apps/frontend/src/app/page.jsx
+++ apps/frontend/src/app/page.jsx
@@ -2,11 +2,9 @@
 
 import React, { useState } from 'react';
 
-import styles from './styles/ads.module.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import useAdsList from './components/hooks/useAdsList';
 import AdsList from './components/ads/adsList/ads-list';
-import AdvDetails from './components/[id]/page';
 
 const Index = () => {
   const [selectedAdv, setSelectedAdv] = useState(null);
@@ -21,7 +19,7 @@ const Index = () => {
   };
 
   return (
-      <AdsList onAdvClick={handleAdClick} />
+      <AdsList onAdvClick={handleAdClick} selectedAd={selectedAdv} selectedAdvId={selectedAdvId} />
   );
 };
 
